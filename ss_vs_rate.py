9c9983a2ac59051cfd85e14ce5e5c327a1a2be10
hookex <cookmycode@gmail.com>
2019-04-30 16:49:10 +0800

    生成图片

--- ss_vs_rate.py
+++ ss_vs_rate.py
@@ -7,13 +7,13 @@ import currency_rate
 import ss
 
 if __name__ == "__main__":
-    start = datetime(2018, 1, 1)
+    start = datetime(2010, 1, 1)
     today = datetime.today()
 
     ssDf = ss.get_ss(start, today)
     rateDf = currency_rate.get_cny(start, today)
 
-    rateDf = rateDf.apply(lambda x: (x - 6) * 4000)
+    rateDf = rateDf.apply(lambda x: (x - 6) * 3000)
 
     # 画纸
     ax1 = ssDf.plot(y="ss", label="ss", legend=True)
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     rateDf.plot(ax=ax1, label="rate", legend=True)
 
     pyplot.xlabel('date')
+
     pyplot.ylabel('ss/currency')
     pyplot.title('ss VS currency rate')
     pyplot.grid(True)
