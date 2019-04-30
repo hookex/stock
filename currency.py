692d6dcaf8d8e8675123d241040da897481a36c5
hookex <cookmycode@gmail.com>
2019-04-30 15:36:32 +0800

    汇率变化图

--- currency.py
+++ currency.py
@@ -5,6 +5,15 @@ import pandas_datareader.data as web
 import requests_cache
 from datetime import timedelta
 
+from matplotlib import pyplot
+
+import seaborn as sns
+
+from datetime import datetime
+
+radius = [1.0, 2.0, 3.0, 4.0]
+area = [3.14159, 12.56636, 28.27431, 50.26544]
+
 expire_after = timedelta(days=7)
 session = requests_cache.CachedSession(
     cache_name='quote',
@@ -19,4 +28,5 @@ def get_cny():
 
 
 if __name__ == "__main__":
-    print(get_cny())
+    get_cny().plot(legend=True)
+    pyplot.show()
